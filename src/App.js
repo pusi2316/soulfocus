c16a9db1ee0b22324d3c39ad1405c9c58705e156
Bence Puschmann <puschmann2@gmail.com>
2025-01-23 23:23:54 -0800

    Adding the whole freaking project. Video background added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { MobileLandingPage } from './MobileLandingPage/landingPage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <MobileLandingPage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MobileLandingPage/MobileLandingPage.jsx
@@ -0,0 +1,263 @@
+import React from "react";
+import arrow12 from "./arrow-1-2.svg";
+import arrow13 from "./arrow-1-3.svg";
+import arrow1 from "./arrow-1.svg";
+import arrow2 from "./arrow-2.svg";
+import copyrightSoulFocusSportsLlc from "./copyright-soul-focus-sports-LLC.svg";
+import facebook from "./facebook.svg";
+import group6 from "./group-6.png";
+import image from "./image.svg";
+import instagram from "./instagram.svg";
+import letSMoveTogether from "./let-s-move-together.svg";
+import linkedin from "./linkedin.svg";
+import meetOurTeamWhoCreatedProducedSupportedSomeOfTheMostImpactfulMassParticipationEventsInTheWorld from "./meet-our-team-who-created-produced-supported-some-of-the-most-impactful-mass-participation-events-in-the-world.svg";
+import menu from "./menu.svg";
+import rectangle2 from "./rectangle-2.png";
+import "./style.css";
+import tempimage0bjyne2 from "./tempimage0bjyne-2.png";
+import tempimagescb1hw1 from "./tempimagescb1hw-1.png";
+import tempimagewco3d63 from "./tempimagewco3d6-3.png";
+import untitled111 from "./untitled-1-1-1.svg";
+import weBuildCommunityThroughThePowerOfMovement from "./we-build-community-through-the-power-of-movement.svg";
+import youtube from "./youtube.svg";
+
+export const MobileLandingPage = () => {
+  return (
+    <div className="mobile-landing-page">
+      <div className="div">
+        <div className="div-2">
+          <div className="div-2">
+            <div className="overlap-group">
+              <div className="rectangle" />
+
+              <img className="img" alt="Rectangle" src={rectangle2} />
+
+              <img
+                className="we-build-community"
+                alt="We build community"
+                src={weBuildCommunityThroughThePowerOfMovement}
+              />
+
+              <p className="text-wrapper">
+                We believe the key to great work is great people. Before
+                starting this business, we were athletes, teammates and coaches.
+              </p>
+
+              <div className="group" />
+
+              <img
+                className="tempimagewcod"
+                alt="Tempimagewcod"
+                src={tempimagescb1hw1}
+              />
+
+              <img
+                className="tempimagewcod-2"
+                alt="Tempimagewcod"
+                src={tempimagewco3d63}
+              />
+            </div>
+          </div>
+
+          <header className="header">
+            <div className="overlap">
+              <div className="rectangle-2" />
+
+              <img className="untitled" alt="Untitled" src={untitled111} />
+
+              <img className="menu" alt="Menu" src={menu} />
+            </div>
+          </header>
+        </div>
+
+        <div className="overlap-2">
+          <div className="rectangle-3" />
+
+          <div className="partners">
+            <div className="overlap-3">
+              <div className="rectangle-4" />
+
+              <div className="text-wrapper-2">WHO WE MOVE WITH</div>
+
+              <img className="group-2" alt="Group" src={group6} />
+
+              <div className="testimonial-card">
+                <div className="text-heading">
+                  <div className="text-heading-2">“Quote”</div>
+                </div>
+
+                <div className="avatar-block">
+                  <div className="avatar" />
+
+                  <div className="info">
+                    <div className="text-wrapper-3">Title</div>
+
+                    <div className="text-wrapper-4">Description</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="gallery">
+            <div className="overlap-4">
+              <img
+                className="tempimagebjyne"
+                alt="Tempimagebjyne"
+                src={tempimage0bjyne2}
+              />
+
+              <img
+                className="tempimagebjyne-2"
+                alt="Tempimagebjyne"
+                src={tempimage0bjyne2}
+              />
+
+              <img
+                className="tempimagescbhw"
+                alt="Tempimagescbhw"
+                src={tempimagescb1hw1}
+              />
+
+              <div className="gallery-slider-left">
+                <div className="overlap-5">
+                  <div className="rectangle-5" />
+
+                  <div className="overlap-group-wrapper">
+                    <div className="overlap-group-2">
+                      <img className="arrow" alt="Arrow" src={arrow13} />
+
+                      <div className="ellipse" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="gallery-slider-right">
+                <div className="overlap-5">
+                  <div className="rectangle-5" />
+
+                  <div className="div-wrapper">
+                    <div className="overlap-group-2">
+                      <img className="arrow-2" alt="Arrow" src={arrow12} />
+
+                      <div className="ellipse" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="rectangle-6" />
+
+          <div className="rectangle-7" />
+
+          <div className="info-soulfocussports"> info@soulfocussports.com</div>
+
+          <p className="p">
+            Whatever the ask, we deliver. We know sports, event production,
+            marketing, grassroots management, and most importantly how to bring
+            the positive energy.
+          </p>
+
+          <div className="socials">
+            <img className="instagram" alt="Instagram" src={instagram} />
+
+            <img className="facebook" alt="Facebook" src={facebook} />
+
+            <img className="linkedin" alt="Linkedin" src={linkedin} />
+
+            <img className="youtube" alt="Youtube" src={youtube} />
+          </div>
+
+          <img
+            className="copyright-soul-focus"
+            alt="Copyright soul focus"
+            src={copyrightSoulFocusSportsLlc}
+          />
+        </div>
+
+        <div className="who-we-are">
+          <div className="text-wrapper-5">WHO WE ARE?</div>
+
+          <img
+            className="meet-our-team-who"
+            alt="Meet our team who"
+            src={
+              meetOurTeamWhoCreatedProducedSupportedSomeOfTheMostImpactfulMassParticipationEventsInTheWorld
+            }
+          />
+
+          <div className="overlap-6">
+            <div className="group-3">
+              <div className="overlap-group-3">
+                <div className="group-4">
+                  <div className="ellipse-2" />
+
+                  <div className="john-doe-position">
+                    John Doe
+                    <br />
+                    Position
+                  </div>
+                </div>
+
+                <div className="group-5">
+                  <div className="ellipse-2" />
+
+                  <div className="john-doe-position">
+                    John Doe
+                    <br />
+                    Position
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="overlap-wrapper">
+              <div className="overlap-7">
+                <div className="rectangle-8" />
+
+                <div className="group-6">
+                  <div className="overlap-group-4">
+                    <img className="arrow-3" alt="Arrow" src={arrow1} />
+
+                    <div className="ellipse-3" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="gallery-slider-left-2">
+              <div className="overlap-7">
+                <div className="rectangle-8" />
+
+                <div className="group-6">
+                  <div className="overlap-group-4">
+                    <img className="arrow-4" alt="Arrow" src={image} />
+
+                    <div className="ellipse-3" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="services-button">
+              <div className="overlap-8">
+                <img className="arrow-5" alt="Arrow" src={arrow2} />
+
+                <div className="text-wrapper-6">Services</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <img
+          className="lets-move-together"
+          alt="Lets move together"
+          src={letSMoveTogether}
+        />
+      </div>
+    </div>
+  );
+};
